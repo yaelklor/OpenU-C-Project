ec43472e25c46d69c3f0f71b0502a32cad5ead4b
yaelklor <79605194+yaelklor@users.noreply.github.com>
2023-07-19 15:02:06 +0300

    Update assembler.c

--- assembler.c
+++ assembler.c
@@ -12,7 +12,7 @@
  * @param filename The filename
  * @return Whether succeeded
  */
-static bool process_file(char *filename);
+static bool process_source_file(char *filename);
 
 /**
  * Entry point.
@@ -25,31 +25,31 @@ int main(int argc, char *argv[]) {
 	for (i = 1; i < argc; ++i) {
 		/* If last process failed and there's another file, break line: */
 		if (!succeeded) puts("");
-		/* Foreach argument (file name), send it for full processing. */
-		succeeded = process_file(argv[i]);
+		/* Send each argument (file name) to full processing. */
+		succeeded = process_source_file(argv[i]);
 	}
 	return 0;
 }
 
-static bool process_file(char *filename) {
+static bool process_source_file(char *filename) {
 	/* Memory address counters */
 	int temp_c;
 	long ic = IC_INIT_VALUE, dc = 0, icf, dcf;
-	bool is_success = TRUE; /* Is succeeded so far */
+	bool succeeded = TRUE; /* Is succeeded so far */
 	char *input_filename;
-	char temp_line[MAX_LINE_LENGTH + 2]; /* Temporary string for storing line, read from file */
+	char temp_line[MAX_LINE_LENGTH + 2]; /* Temp string for storing line, read from file */
 	FILE *file_des; /* Current assembly file descriptor to process */
 	long data_img[CODE_ARR_IMG_LENGTH]; /* Contains an image of the machine code */
 	machine_word *code_img[CODE_ARR_IMG_LENGTH];
-	/* Our symbol table */
+	/* The symbol table */
 	table symbol_table = NULL;
-	/* Our mcro lines table */
+	/* The mcro lines table */
 	macro_table mcro_table = NULL;
-	/* Our lines after mcro processed table */
+	/* The lines after mcro processed table */
 	macro_table processed_mcro_lines_table = NULL; 
 	bool *found_macro = FALSE; /* If found macro: flag will turn to TRUE, flag will turn FALSE once mcro lines added to table */
 	char *macro_name[MAX_LINE_LENGTH]; /* If found macro: macro_name=current macro name, macro_name=\0 once mcro lines added to table */
-	line_info curr_line_info;
+	line_info current_line;
 
 	/* Concat extensionless filename with .as extension */
 	input_filename = strallocat(filename, ".as");
@@ -64,36 +64,37 @@ static bool process_file(char *filename) {
 	}
 
 	/*Start parsing macros: */
-	curr_line_info.file_name = input_filename;
-	curr_line_info.content = temp_line; /* We use temp_line to read from the file, but it stays at same location. */
-	/* Read line - stop if read failed (when NULL returned) - usually when EOF. increase line counter for error printing. */
-	for (curr_line_info.line_number = 1; fgets(temp_line, MAX_LINE_LENGTH + 2, file_des) != NULL; curr_line_info.line_number++) {
+	current_line.file_name = input_filename;
+	current_line.content = temp_line; /* We use temp_line to read from the file, but it stays at same location. */
+	/* Read line - stop if read failed (when NULL returned) - usually when EOF. 
+ 	increase line counter for error printing. */
+	for (current_line.line_number = 1; fgets(temp_line, MAX_LINE_LENGTH + 2, file_des) != NULL; current_line.line_number++) {
 		/* if line too long, the buffer doesn't include the '\n' char OR the file isn't on end. */
 		if (strchr(temp_line, '\n') == NULL && !feof(file_des)) {
 			/* Print message and prevent further line processing, as well as second pass.  */
-			printf_line_error(curr_line_info, "Line too long to process. Maximum line length should be %d.", MAX_LINE_LENGTH);
-			is_success = FALSE;
+			printf_line_error(current_line, "Line too long to process. Maximum line length should be %d.", MAX_LINE_LENGTH);
+			succeeded = FALSE;
 			/* skip leftovers */
 			do {
 				temp_c = fgetc(file_des);
 			} while (temp_c != '\n' && temp_c != EOF);
 		} else {
-			if (!process_line_macros(curr_line_info, &mcro_table, &processed_mcro_lines_table, &found_macro, macro_name)) {
-				if (is_success) {
-					is_success = FALSE;
+			if (!process_line_macros(current_line, &mcro_table, &processed_mcro_lines_table, &found_macro, macro_name)) {
+				if (succeeded) {
+					succeeded = FALSE;
 				}
 			}
 		}
 	}
 
 	/* write processed_mcro_lines_table to .am file */
-	if (is_success) {
+	if (succeeded) {
 		/* Concat extensionless filename with .am extension */
-		is_success= write_macro_table_to_file(processed_mcro_lines_table, filename, ".am");
+		succeeded= write_macro_table_to_file(processed_mcro_lines_table, filename, ".am");
 	}
 
 	/* if proccess mcro lines succeeded and .am file created successfully: start the first pass */
-	if (is_success) {
+	if (succeeded) {
 		input_filename = strallocat(filename, ".am");
 		/* Open file, skip on failure */
 		file_des = fopen(input_filename, "r");
@@ -105,24 +106,24 @@ static bool process_file(char *filename) {
 		}
 
 		/* start first pass: */
-		curr_line_info.file_name = input_filename;
-		curr_line_info.content = temp_line; /* We use temp_line to read from the file, but it stays at same location. */
+		current_line.file_name = input_filename;
+		current_line.content = temp_line; /* We use temp_line to read from the file, but it stays at same location. */
 		/* Read line - stop if read failed (when NULL returned) - usually when EOF. increase line counter for error printing. */
-		for (curr_line_info.line_number = 1; fgets(temp_line, MAX_LINE_LENGTH + 2, file_des) != NULL; curr_line_info.line_number++) {
+		for (current_line.line_number = 1; fgets(temp_line, MAX_LINE_LENGTH + 2, file_des) != NULL; current_line.line_number++) {
 			/* if line too long, the buffer doesn't include the '\n' char OR the file isn't on end. */
 			if (strchr(temp_line, '\n') == NULL && !feof(file_des)) {
 				/* Print message and prevent further line processing, as well as second pass.  */
-				printf_line_error(curr_line_info, "Line too long to process. Maximum line length should be %d.", MAX_LINE_LENGTH);
-				is_success = FALSE;
+				printf_line_error(current_line, "Line too long to process. Maximum line length should be %d.", MAX_LINE_LENGTH);
+				succeeded = FALSE;
 				/* skip leftovers */
 				do {
 					temp_c = fgetc(file_des);
 				} while (temp_c != '\n' && temp_c != EOF);
 			} else {
-				if (!process_line_fpass(curr_line_info, &ic, &dc, code_img, data_img, &symbol_table)) {
-					if (is_success) {
+				if (!process_line_fpass(current_line, &ic, &dc, code_img, data_img, &symbol_table)) {
+					if (succeeded) {
 						icf = -1;
-						is_success = FALSE;
+						succeeded = FALSE;
 					}
 				}
 			}
@@ -133,8 +134,8 @@ static bool process_file(char *filename) {
 		dcf = dc;
 	}
 
-	/* if the first pass didn't fail its time to start the second pass */
-	if (is_success) {
+	/* if the first pass didn't fail it's time to start the second pass */
+	if (succeeded) {
 		ic = IC_INIT_VALUE;
 
 		/* Now is time to add IC to each DC for each of the data symbols in table */
@@ -142,18 +143,18 @@ static bool process_file(char *filename) {
 
 		rewind(file_des); /* Start from beginning of file again */
 
-		for (curr_line_info.line_number = 1; !feof(file_des); curr_line_info.line_number++) {
+		for (current_line.line_number = 1; !feof(file_des); current_line.line_number++) {
 			int i = 0;
 			fgets(temp_line, MAX_LINE_LENGTH, file_des); /* Get line */
 			MOVE_TO_NOT_WHITE(temp_line, i)
 			if (code_img[ic - IC_INIT_VALUE] != NULL || temp_line[i] == '.')
-				is_success &= process_line_spass(curr_line_info, &ic, code_img, &symbol_table);
+				succeeded &= process_line_spass(current_line, &ic, code_img, &symbol_table);
 		}
-			printf("is_success after second pass %d\n", is_success);
+			printf("succeeded after second pass %d\n", succeeded);
 			/* Write files if second pass succeeded */
-			if (is_success) {
+			if (succeeded) {
 				/* Everything was done. Write to *filename.ob/.ext/.ent */
-				is_success = write_output_files(code_img, data_img, icf, dcf, filename, symbol_table);
+				succeeded = write_output_files(code_img, data_img, icf, dcf, filename, symbol_table);
 			}
 	}
 
@@ -169,6 +170,6 @@ static bool process_file(char *filename) {
 	/* Free code & data buffer contents */
 	free_code_image(code_img, icf);
 
-	/* return whether every assembling succeeded */
-	return is_success;
+	/* return whether processing succeeded */
+	return succeeded;
 }
