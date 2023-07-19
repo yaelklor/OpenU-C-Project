e74619a022d7531efe42a6a192d4923d4488ffb0
yaelklor <79605194+yaelklor@users.noreply.github.com>
2023-07-19 15:30:53 +0300

    Update instructions.c

--- instructions.c
+++ instructions.c
@@ -1,7 +1,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "utils.h"
+#include "utilities_functions.h"
 
 
 /* Returns the first instruction from the specified index. if no such one, returns NONE */
